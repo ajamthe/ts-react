90a2cd1d611b0e394ca7a6fc242db0af5827a1ef
Ashish Jamthe <ajamthe@gmail.com>
2020-10-24 09:46:14 +1100

    First component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import TodoList from "./components/TodoList";
 
 // React.FC is a shortform form React.FunctionComponent
 const App: React.FC = () => {
+  const todos = [{id: 't1', text: 'Finish the course'}];
+
   return (
     <div className="App">
+      <TodoList items={todos}/>
     </div>
   );
 }
